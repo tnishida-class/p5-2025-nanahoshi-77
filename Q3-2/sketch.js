4724191a5869a63e4f5640b04980ff63d84a859e
nanahoshi-77 <yoshyasmine@gmail.com>
2025-11-24 20:07:13 +0900

    演習課題

--- Q3-2/sketch.js
+++ Q3-2/sketch.js
@@ -1,7 +1,9 @@
 // 2D アニメーションゲームのようなインタラクション
 let x, y;
 let vx, vy;
-const g = 1;
+const g = 1 ;
+let size;
+let groundY;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
@@ -9,6 +11,8 @@ function setup(){
   y = height / 2;
   vx = 0;
   vy = 0;
+ size = height * 0.1; // キャラクターのサイズ
+ groundY = height * 0.8;
 }
 
 function windowResized(){
@@ -17,20 +21,43 @@ function windowResized(){
 
 function draw(){
   background(160, 192, 255);
-  const size = height * 0.1; // キャラクターのサイズ
-
   // 地面を描く
-  const groundY = height * 0.8;
+  groundY = height * 0.8;
   fill(64, 192, 64);
   rect(0, groundY, width, height - groundY);
+  size = height * 0.1; // キャラクターのサイズ
 
   // BLANK[1] キャラクターの左右移動
+  let speed = 5;
+  //Shiftキーを押している間だけスピードアップ
+  if (keyIsDown(SHIFT)) {
+   speed = 10
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    vx = -speed ;
+  } else if (keyIsDown (RIGHT_ARROW)){
+    vx = speed ;
+  } else {
+      vx = 0;
+    }
 
   // BLANK[2] 重力とジャンプ
+  vy += g; //重力を加える
+  y += vy; 
+
+  //地面についたか判定
+  if (y + size / 2 >= groundY) {
+    y = groundY - size / 2;
+     vy = 0;
+  }
 
   // 速くなりすぎないように制限
-  vx = constrain(vx, -20, 20);
+   vx = constrain(vx, -20, 20);
   vy = constrain(vy, -20, 20);
+
+  x = constrain (x, size / 2, width - size / 2 )
+  y = constrain (y, size / 2, groundY - size / 2)
 
   // 位置を更新
   x += vx;
@@ -40,3 +67,10 @@ function draw(){
   fill(0);
   ellipse(x, y, size, size);
 }
+
+function keyPressed (){
+  //スペースキーでジャンプ（空中ジャンプ不可）
+  if (key === ' ' && (y + size / 2 >= groundY)){
+    vy = -20; //ジャンプ力     
+  }
+}      
